3b2b6d5bb3a94684a56cd082ad27f8c4cdbf5f9c
Swaroop Sanjay Patil <swaroop@apl.com>
2022-08-29 17:11:37 +0530

    Animals

---
+++ Animal/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using Animal.Models;
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+
+namespace Animal.Controllers
+{
+    public class HomeController : Controller
+    {
+
+
+        string conString = "Data Source = LTP_RD_411; Initial Catalog = test; Integrated Security = True;";
+
+        public IActionResult AllData()
+        {
+            string query = "select * from animals";
+            SqlConnection con = new SqlConnection(conString);
+            var list = con.Query<Class>(query).ToList();
+            return View(list);
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Insert(Class a)
+        {
+            string insert = "insert into animals (Name, City) values(@name, @city)";
+            Console.WriteLine(a.Name);
+            IDbConnection con = new SqlConnection(conString);
+            con.Execute(insert, a);
+            return Redirect ("/");
+        }
+
+        public IActionResult DeleteProcess(int id)
+        {
+            string delete = "delete from animals where AID = @AID";
+            IDbConnection con = new SqlConnection(conString);
+            con.Execute(delete, new {AID = id});
+            return Redirect ("/");
+        }
+
+        public IActionResult EditProcess(int id)
+        {
+            string query = "select * from animals where AID = @AID";
+            SqlConnection con = new SqlConnection(conString);
+            var a = con.Query<Class>(query, new { AID = id }).FirstOrDefault();
+            
+            return View(a);
+        }
+
+    }
+}
---
+++ Animal/Models/Class.cs
@@ -0,0 +1,12 @@
+﻿namespace Animal.Models
+{
+    public class Class
+    {
+        public int AID { get; set; }
+
+        public string Name { get; set; }
+
+        public string City { get; set; }
+
+    }
+}
